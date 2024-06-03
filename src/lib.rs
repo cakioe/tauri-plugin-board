607ed16009b85d2f61748f2b223bece5ea0618d7
cleveng <cleveng@gmail.com>
2024-06-03 17:55:39 +0800

    update config of cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use tauri::{plugin::{Builder, TauriPlugin}, Runtime};
 /// Initializes the plugin.
 pub fn init<R: Runtime>() -> TauriPlugin<R> {
     Builder::new("board")
-        .setup(|app, api| {
+        .setup(|_, api| {
             #[cfg(target_os = "android")]
                 let handle = api.register_android_plugin("com.plugin.board", "DevicePlugin")?;
             Ok(())
