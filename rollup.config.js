57f22f45f3cfc4305a653f155643cbdf82c216d8
cleveng <cleveng@gmail.com>
2024-06-07 16:12:43 +0800

    add @rollup/plugin-terser plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,22 @@
 import typescript from '@rollup/plugin-typescript'
+import terser from '@rollup/plugin-terser'
 
 export default {
   input: 'webview-src/index.ts',
-  plugins: [typescript({
-    include: ['webview-src/**/*.ts', 'webview-src/**/*.tsx']
-  })],
+  plugins: [
+    typescript({
+      include: ['webview-src/**/*.ts', 'webview-src/**/*.tsx'],
+      tsconfig: 'tsconfig.json'
+    }),
+    terser()
+  ],
   output: [{
-    file: 'webview-dist/index.js',
-    format: 'es'
+    file: 'webview-dist/index.mjs',
+    format: 'es',
+    sourcemap: true
   }, {
     file: 'webview-dist/index.cjs',
-    format: 'cjs'
+    format: 'cjs',
+    sourcemap: true
   }]
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
 use tauri::{plugin::{Builder, TauriPlugin}, Runtime};
 
+#[cfg(target_os = "android")]
+const PLUGIN_IDENTIFIER: &str = "com.plugin.board";
+
 /// Initializes the plugin.
 pub fn init<R: Runtime>() -> TauriPlugin<R> {
     Builder::new("board")
         .setup(|_, _api| {
             #[cfg(target_os = "android")]
-                let handle = _api.register_android_plugin("com.plugin.board", "DevicePlugin")?;
+            let handle = _api.register_android_plugin(PLUGIN_IDENTIFIER, "DevicePlugin")?;
             Ok(())
         })
         .build()
